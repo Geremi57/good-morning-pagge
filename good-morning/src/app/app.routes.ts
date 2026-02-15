d7684e136013e2c19a61f18b97dc72723413a131
Geremi57 <geremiwanga57@gmail.com>
2026-02-16 00:34:04 +0300

    init angular project

---
+++ good-morning/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { FormComponent } from './form-component/form-component';
+import { Component } from '@angular/core';
+export const routes: Routes = [
+{
+    path: 'form',
+    component: FormComponent,
+
+},
+];
